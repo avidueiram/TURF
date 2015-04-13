8c31ab84ed3db4d4dcc2acad240af171784c7348
Towercel <alfredo.vidueiram@mayor.cl>
2015-04-12 23:19:58 -0400

    Viktor App Update

---
+++ Viktor/src/viktor/GlobalDef.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viktor;
+
+/**
+ *
+ * @author Alfredo
+ */
+public class GlobalDef
+{
+    public static final String DEF_HOSTNAME = "localhost";
+    public static final String DEF_DATABASE = "topurf";
+    public static final String DEF_USERNAME = "root";
+    public static final String DEF_PASSWORD = "";
+    
+    public static final String DEF_API_KEY = "<API_KEY>";
+    
+    public static final long DEF_START_TIME = 1427865900;
+}
